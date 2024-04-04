85f19f4ad21bc696f7f48e533016631bb22cd9f2
Gurshameerjot Singh <gurshameerjotsingh1601@gmail.com>
2024-04-04 11:23:16 +0530

    More responses added in chatbot

--- To-Do-List-Sorted/js/web.js
+++ To-Do-List-Sorted/js/web.js
@@ -11,11 +11,17 @@ document.addEventListener('DOMContentLoaded', function () {
     function botReply(userMessage) {
         // You can customize the bot's responses here
         const responses = {
-            'hi':'hello there! how can i help you?',
-            'hello': 'Hi there! whats your name? ',
+            'hi': 'Hello there! How can I assist you?',
+            'hello': "Hi there! What's your name?",
             'how are you': 'I am just a bot, but thanks for asking!',
+            'what is your purpose': 'My purpose is to assist you with any questions or tasks you have.',
+            'who created you': 'I was created by a team of developers at Taskmasters.',
+            'what time is it': `I'm sorry, I don't have access to real-time information.`,
+            'tell me a joke': 'Why don’t scientists trust atoms? Because they make up everything!',
+            'what is the meaning of life': `That's a tough one. The meaning of life is subjective and varies from person to person.`,
             'default': "I'm sorry, I didn't understand that.",
         };
+        
 
         const normalizedUserMessage = userMessage.toLowerCase();
         const response = responses[normalizedUserMessage] || responses['default'];
